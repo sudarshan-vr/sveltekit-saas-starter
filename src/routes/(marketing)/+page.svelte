1caa8f01b00c643d07573925352b65bfa1374dfe
sudarshan vr <104901361+sudarshan-vr@users.noreply.github.com>
2025-09-30 04:49:59 +0530

    Update +page.svelte

--- src/routes/(marketing)/+page.svelte
+++ src/routes/(marketing)/+page.svelte
@@ -235,7 +235,7 @@
       <div
         class="text-xl md:text-3xl font-bold bg-clip-text text-transparent bg-linear-to-r from-primary to-accent mb-3 md:mb-7 pb-1"
       >
-        Craftuary
+        Craftuary (Site Under Maintainance)
       </div>
 
       <div
